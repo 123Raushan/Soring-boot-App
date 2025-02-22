839d3feb25b56dbbfb1444f60efed267a40b7c88
123Raushan <117634027+123Raushan@users.noreply.github.com>
2025-02-22 13:08:29 +0530

    Update java-quiz.js

--- java-quiz.js
+++ java-quiz.js
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', function () {
     // Function to fetch questions from the API
     async function fetchQuestions() {
         try {
-            const response = await fetch(`http://localhost:9090/api/${title}_question`);
+            const response = await fetch(`https://question-quiz-production.up.railway.app/api/${title}_question`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
